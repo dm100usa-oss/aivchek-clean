ace18f748527b905c6e2c55e51fa3acfa122091e
dm100usa-oss <dm100usa@gmail.com>
2025-10-03 15:58:52 -0400

    Update pdf.ts

--- lib/pdf.ts
+++ lib/pdf.ts
@@ -1,12 +1,13 @@
-import React from "react";
 import { renderToBuffer } from "@react-pdf/renderer";
+import React from "react";
 import ReportPDF_Owner from "@/components/pdf/ReportPDF_Owner";
 import ReportPDF_Developer from "@/components/pdf/ReportPDF_Developer";
+import { AnalyzeResult } from "@/lib/analyze";
 
 export async function generateReports(
   url: string,
   date: string,
-  analysis: any
+  analysis: AnalyzeResult
 ) {
   const ownerBuffer = await renderToBuffer(
     React.createElement(ReportPDF_Owner, { url, date, ...analysis })
